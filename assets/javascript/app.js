ecceeff68bc1db163c43fe7f3e70c0df9e180ec9
Ovieh <ovieh.mosley@gmail.com>
2017-09-14 11:09:04 -0400

    Start of game logic.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
 "use strict";
 
-$(function () {
+// $(function () {
     var score = 0;
-    var questionsRemaining = 10;
     var questionCount = 0;
     var questionTime = 21;
     var intervalId;
@@ -34,6 +33,9 @@ $(function () {
             $("#selection_3").html(questions[questionCount].choices[2]);
             $("#selection_4").html(questions[questionCount].choices[3]);
         }
+        else {
+            displayEnd();
+        }
     }
     function startTimer(){
         intervalId = setInterval(decrement, 1000);
@@ -50,6 +52,11 @@ $(function () {
     function stop() {
         clearInterval(intervalId);
     }
+
+    function displayEnd() {
+
+    }
+
     $("#start").on("click", function () {
         $("#start").hide();
         startTimer();
@@ -58,6 +65,14 @@ $(function () {
     });
 
     $(".selection").on("click", function(){
-        console.log($(this).attr("id"));
+        //using weak comparison on purpose
+        if(($(this).attr("data-answer-number")) == questions[questionCount].answer ){
+            console.log("right anser");
+            score++;
+            questionCount++;
+            displayQuestion();
+            questionTime = 21;
+            
+        } 
     });
-});
+// });
