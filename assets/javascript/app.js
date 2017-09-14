9e4791eb7a83d8dba6d3de3922226763512dbb0b
Ovieh <ovieh.mosley@gmail.com>
2017-09-14 10:30:07 -0400

    Load screen and inital question up.

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+"use strict";
+
+$(function () {
+    var score = 0;
+    var questionsRemaining = 10;
+    var questionCount = 0;
+    var questionTime = 21;
+    var intervalId;
+
+
+    var questions = [{
+            prompt: "Who built the Wall?",
+            choices: ["Ned Stark", "Brandon Stark", "Aegon the IV", "Tormund Giantsbane"],
+            answer: 1,
+        },
+        {
+            prompt: "Where is the Citadel located?",
+            choices: ["Kings Landing", "Harrenhall", "Oldtown", "Winterfell"],
+            answer: 2
+        },
+        {
+            prompt: "Who is not one of the Seven Gods of Westeros?",
+            choices: ["The Drowned God", "The Mother", "The Stranger", "The Warrior"],
+            answer: 0
+        }
+
+    ];
+
+    function displayQuestion() {
+        if(questionCount < questions.length) {
+            $("#prompt").html(questions[questionCount].prompt);
+            $("#selection_1").html(questions[questionCount].choices[0]);
+            $("#selection_2").html(questions[questionCount].choices[1]);
+            $("#selection_3").html(questions[questionCount].choices[2]);
+            $("#selection_4").html(questions[questionCount].choices[3]);
+        }
+    }
+    function startTimer(){
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+        questionTime--;
+        $("#show-time").html(questionTime);
+        if(questionTime === 0) {
+            stop();
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+    }
+    $("#start").on("click", function () {
+        $("#start").hide();
+        startTimer();
+        
+        displayQuestion();
+    });
+
+    $(".selection").on("click", function(){
+        console.log($(this).attr("id"));
+    });
+});
